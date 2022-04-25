6f198a15d7f66f072ff9dc54d4606ebdb0d67dc8
rafaeltardivo <rafaeldt.br@gmail.com>
2022-04-25 19:06:44 -0300

    Add get_environment_variables to utils

--- app/utils.py
+++ app/utils.py
@@ -20,3 +20,15 @@ def load_environment_variable(variable: str) -> str:
         raise MissingEnvironmentVariableError(variable)
 
     return value
+
+
+def get_environment_variables(variables: list) -> dict:
+    """Retrieves environment variables.
+    Args:
+        variables (list): variable names.
+    Returns:
+        dict: dictionary containing all requested environment variables.
+    Raises:
+        MissingEnvironmentVariableError: if one of the variables is missing.
+    """
+    return {var: load_environment_variable(var) for var in variables}
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,16 +3,24 @@ import pytest
 from unittest import mock
 
 from app.exceptions import MissingEnvironmentVariableError
-from app.utils import load_environment_variable
+from app.utils import load_environment_variable, get_environment_variables
 
 
-@mock.patch.dict(os.environ, {"FOO": "BAR"})
 def test_load_environment_variable():
-    var = load_environment_variable("FOO")
-    assert var == "BAR"
+    key, value = "FOO", "BAR"
+
+    with mock.patch.dict(os.environ, {key: value}):
+        var = load_environment_variable(key)
+        assert var == value
 
 
 def test_load_missing_environment_variable():
-
     with pytest.raises(MissingEnvironmentVariableError):
         load_environment_variable("MISSING")
+
+
+def test_get_environment_variables():
+    env_vars = {"ONE": "1", "TWO": "2"}
+    with mock.patch.dict(os.environ, env_vars):
+        ret = get_environment_variables(env_vars.keys())
+        assert ret == env_vars
