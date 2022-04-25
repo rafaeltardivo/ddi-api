3b5ff7c43ab1658ebab4ca71517e78782375eeb6
rafaeltardivo <rafaeldt.br@gmail.com>
2022-04-25 19:18:26 -0300

    Add create_app factory to api

--- app/main.py
+++ app/main.py
@@ -1,12 +1,40 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+
+from .utils import get_environment_variables
 
 app = FastAPI()
 
 
+def create_app():
+    """Application factory."""
+
+    app = FastAPI()
+
+    @app.middleware("http")
+    async def add_credentials_middleware(request: Request, call_next):
+        request.state.env_vars = get_environment_variables(
+            [
+                "INFLUXDB_HOST",
+                "INFLUXDB_PORT",
+                "DOCKER_INFLUXDB_INIT_ADMIN_TOKEN",
+                "DOCKER_INFLUXDB_INIT_ORG",
+                "DOCKER_INFLUXDB_INIT_BUCKET",
+            ]
+        )
+        response = await call_next(request)
+        return response
+
+    return app
+
+
+app = create_app()
+
+
 @app.get("/health")
-async def health():
+async def health(request: Request):
+    print(request.state.env_vars)
     return {}
 
 
@@ -16,5 +44,5 @@ async def create_event():
 
 
 @app.get("/devices/histogram/{device_id}")
-async def device_histogram():
+async def get_device_histogram():
     return {}
