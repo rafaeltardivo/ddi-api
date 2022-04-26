4de6ac1e54a9e1b905c0a0e1649072ed2bf03350
rafaeltardivo <rafaeldt.br@gmail.com>
2022-04-26 07:42:03 -0300

    Add get_histogram use case

--- app/use_cases.py
+++ app/use_cases.py
@@ -1,24 +1,43 @@
-from influxdb_client import Point
+from collections import Counter
 
 from .schemas import Event
-from .repository import add_event
+from .repository import add_event, get_status_frequency
+from .utils import get_formatted_flux_query
 
 
 async def create_event(credentials: dict, bucket: str, event: Event) -> bool:
     """Create a new device event.
     Args:
         credentials (dict): dict containing database credentials.
-        bucket (dict): dict containing database credentials.
+        bucket (str): target bucket.
         event: (event): serialized Event object.
     Returns:
         bool: True if the event was successfully created, False otherwise.
     """
-    point = (
-        Point("sensors")
-        .tag("deviceId", event.device_id)
-        .field("status", event.status)
-        .time(event.timestamp)
-    )
+    data = {
+        "measurement": "sensors",
+        "tags": {"deviceId": event.device_id},
+        "fields": {"status": event.status},
+        "time": event.timestamp,
+    }
+    result = await add_event(credentials, bucket, data)
 
-    result = await add_event(credentials, bucket, point)
     return result
+
+
+async def get_histogram(
+    credentials: dict, bucket: str, query_parameters: dict
+) -> tuple:
+    """Get device histogram.
+    Args:
+        credentials (dict): dict containing database credentials.
+        bucket (str): target bucket.
+        query_parameters (dict): query parameters.
+    Returns:
+        dict: histogram (x=key, y=value).
+    """
+    query = get_formatted_flux_query(bucket, query_parameters)
+    frequency = await get_status_frequency(credentials, query)
+
+    histogram = Counter(frequency)
+    return histogram
--- tests/test_use_cases.py
+++ tests/test_use_cases.py
@@ -1,9 +1,12 @@
+from collections import Counter
+
 from unittest.mock import MagicMock, patch
 
 import pytest
 
-from app.use_cases import create_event
+from app.use_cases import create_event, get_histogram
 from app.schemas import Event
+from app.utils import get_formatted_flux_query
 
 
 class AsyncMock(MagicMock):
@@ -22,5 +25,35 @@ async def test_create_event(mocked_add_event):
 
     res = await create_event(db_credentials, bucket, event)
 
-    mocked_add_event.assert_called_once()
+    mocked_add_event.assert_called_once_with(
+        db_credentials,
+        bucket,
+        {
+            "measurement": "sensors",
+            "tags": {"deviceId": "sensor-1"},
+            "fields": {"status": "ON"},
+            "time": "2020-01-02T03:44:02",
+        },
+    )
     assert res is True
+
+
+@pytest.mark.asyncio
+@patch("app.use_cases.get_status_frequency", new_callable=AsyncMock)
+async def test_get_histogram(mocked_get_status_frequency):
+    mocked_frequency = ["ON", "ON", "OFF", "ACTIVE"]
+    mocked_get_status_frequency.return_value = mocked_frequency
+
+    db_credentials = {"url": "mocked_url", "token": "mocked_token", "org": "mocked_org"}
+    bucket = "test_bucket"
+    query_parameters = {
+        "deviceId": "sensor-1",
+        "start": "2020-01-02T03:44:02",
+        "end": "2020-01-02T03:45:02",
+    }
+
+    res = await get_histogram(db_credentials, bucket, query_parameters)
+    mocked_get_status_frequency.assert_called_once_with(
+        db_credentials, get_formatted_flux_query(bucket, query_parameters)
+    )
+    assert res == Counter(mocked_frequency)
