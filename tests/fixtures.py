36baa76478f1c19e84414b2d883d21deac3561b7
rafaeltardivo <rafaeldt.br@gmail.com>
2022-04-26 09:03:43 -0300

    Update test fixtures

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -19,14 +19,44 @@ def event():
 
 
 @pytest.fixture
+def start():
+    return "2020-01-02T03:44:00"
+
+
+@pytest.fixture
+def stop():
+    return "2020-01-02T03:48:00"
+
+
+@pytest.fixture
+def device_id():
+    return "sensor-1"
+
+
+@pytest.fixture
 def query_parameters():
     return {
         "deviceId": "sensor-1",
-        "start": "2020-01-02T03:44:02",
-        "end": "2020-01-02T03:45:02",
+        "start": "2020-01-02T03:44:00",
+        "stop": "2020-01-02T03:48:00",
     }
+
+
+@pytest.fixture
+def qs(start, stop):
+    return f"start={start}&stop={stop}"
 
 
 @pytest.fixture
 def frequency():
     return ["ON", "ON", "OFF", "ACTIVE"]
+
+
+@pytest.fixture
+def env_vars():
+    return {
+        "INFLUXDB_HOST": "mocked_influxdb",
+        "INFLUXDB_PORT": "8086",
+        "DOCKER_INFLUXDB_INIT_ADMIN_TOKEN": "mocked_token",
+        "DOCKER_INFLUXDB_INIT_ORG": "mocked_org",
+    }
