ab48afafcb7cc917b17fc477498d4bc44560d968
rafaeltardivo <rafaeldt.br@gmail.com>
2022-04-26 01:06:20 -0300

    Remove get_health from use cases

--- app/main.py
+++ app/main.py
@@ -2,50 +2,49 @@ from http import HTTPStatus
 
 from fastapi import FastAPI, Request
 
+from .schemas import Event
 from .utils import get_environment_variables, get_db_credentials
+from .use_cases import create_event
+
+from influxdb_client.client.influxdb_client_async import InfluxDBClientAsync
 
 app = FastAPI()
 
 
-def create_app():
-    """Application factory."""
-
-    app = FastAPI()
-
-    @app.middleware("http")
-    async def env_vars_middleware(request: Request, call_next):
-        """Middleware for state persistence of env vars."""
-        request.state.env_vars = get_environment_variables(
-            [
-                "INFLUXDB_HOST",
-                "INFLUXDB_PORT",
-                "DOCKER_INFLUXDB_INIT_ADMIN_TOKEN",
-                "DOCKER_INFLUXDB_INIT_ORG",
-                "DOCKER_INFLUXDB_INIT_BUCKET",
-            ]
-        )
-        response = await call_next(request)
-        return response
-
-    return app
-
-
-app = create_app()
+@app.middleware("http")
+async def env_vars_middleware(request: Request, call_next):
+    """Middleware for state persistence of env vars."""
+    request.state.env_vars = get_environment_variables(
+        [
+            "INFLUXDB_HOST",
+            "INFLUXDB_PORT",
+            "DOCKER_INFLUXDB_INIT_ADMIN_TOKEN",
+            "DOCKER_INFLUXDB_INIT_ORG",
+            "DOCKER_INFLUXDB_INIT_BUCKET",
+        ]
+    )
+    response = await call_next(request)
+    return response
 
 
 @app.get("/health")
 async def health(request: Request):
     db_credentials = get_db_credentials(request.state.env_vars)
-    return {}
+    async with InfluxDBClientAsync(**db_credentials) as client:
+        ready = await client.ping()
+
+    return {"api": True, "db": ready}
 
 
 @app.post("/devices/events", status_code=HTTPStatus.CREATED)
-async def create_event():
+async def create_device_event(request: Request, event: Event):
     db_credentials = get_db_credentials(request.state.env_vars)
-    return {}
+    bucket = request.state.env_vars["DOCKER_INFLUXDB_INIT_BUCKET"]
+    result = await create_event(db_credentials, bucket, event)
+
+    return {"success": result}
 
 
 @app.get("/devices/histogram/{device_id}")
-async def get_device_histogram():
-    db_credentials = get_db_credentials(request.state.env_vars)
+async def get_device_histogram(request: Request):
     return {}
--- app/use_cases.py
+++ app/use_cases.py
@@ -1,21 +1,7 @@
-from influxdb_client.client.influxdb_client_async import InfluxDBClientAsync
 from influxdb_client import Point
 
 from .schemas import Event
 from .repository import add_event
-
-
-async def get_health(credentials: dict) -> tuple:
-    """Service healthcheck.
-    Args:
-        credentials (dict): dict containing database credentials.
-    Returns:
-        tuple: tuple containing api and db statuses.
-    """
-    async with InfluxDBClientAsync(**credentials) as client:
-        ready = await client.ping()
-
-    return True, ready
 
 
 async def create_event(credentials: dict, bucket: str, event: Event) -> bool:
