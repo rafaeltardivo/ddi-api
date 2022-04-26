4caad5172f75ae9b3d7e52258f1275ba8c421490
rafaeltardivo <rafaeldt.br@gmail.com>
2022-04-26 01:07:03 -0300

    Update tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,7 @@
+import pytest
+
 from http import HTTPStatus
+from unittest.mock import MagicMock, patch
 from fastapi.testclient import TestClient
 
 from app.main import app
@@ -6,16 +9,38 @@ from app.main import app
 client = TestClient(app)
 
 
-def test_health():
+class AsyncMock(MagicMock):
+    async def __call__(self, *args, **kwargs):
+        return super().__call__(*args, **kwargs)
+
+
+@pytest.mark.asyncio
+@patch("app.main.InfluxDBClientAsync.ping", new_callable=AsyncMock)
+async def test_health(mocked_ping):
+    mocked_ping.return_value = True
+
     response = client.get("/health")
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {}
+    assert response.json() == {"api": True, "db": True}
 
 
-def test_create_event():
-    response = client.post("/devices/events")
+@pytest.mark.asyncio
+@patch("app.main.create_event", new_callable=AsyncMock)
+async def test_create_event(mocked_create_event):
+    mocked_create_event.return_value = True
+
+    response = client.post(
+        "/devices/events",
+        json={
+            "deviceId": "sensor-1",
+            "timestamp": "2020-01-02T03:44:02",
+            "pressure": 212.0,
+            "status": "ON",
+            "temperature": 230,
+        },
+    )
     assert response.status_code == HTTPStatus.CREATED
-    assert response.json() == {}
+    assert response.json() == {"success": True}
 
 
 def test_get_device_histogram():
--- tests/test_use_cases.py
+++ tests/test_use_cases.py
@@ -2,24 +2,13 @@ from unittest.mock import MagicMock, patch
 
 import pytest
 
-from app.use_cases import get_health, create_event
+from app.use_cases import create_event
 from app.schemas import Event
 
 
 class AsyncMock(MagicMock):
     async def __call__(self, *args, **kwargs):
         return super().__call__(*args, **kwargs)
-
-
-@pytest.mark.asyncio
-@patch("app.use_cases.InfluxDBClientAsync.ping", new_callable=AsyncMock)
-async def test_get_health(mocked_ping):
-    mocked_ping.return_value = True
-    db_credentials = {"url": "mocked_url", "token": "mocked_token", "org": "mocked_org"}
-    res = await get_health(db_credentials)
-
-    mocked_ping.assert_called_once()
-    assert res == (True, True)
 
 
 @pytest.mark.asyncio
