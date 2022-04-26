579fa55909cbe70d837271f068f777e390a0c976
rafaeltardivo <rafaeldt.br@gmail.com>
2022-04-26 08:24:30 -0300

    Add fixtures to use cases tests

--- tests/test_use_cases.py
+++ tests/test_use_cases.py
@@ -7,6 +7,7 @@ import pytest
 from app.use_cases import create_event, get_histogram
 from app.schemas import Event
 from app.utils import get_formatted_flux_query
+from .fixtures import db_credentials, bucket, event, query_parameters, frequency
 
 
 class AsyncMock(MagicMock):
@@ -16,44 +17,23 @@ class AsyncMock(MagicMock):
 
 @pytest.mark.asyncio
 @patch("app.use_cases.add_event", new_callable=AsyncMock)
-async def test_create_event(mocked_add_event):
+async def test_create_event(mocked_add_event, db_credentials, bucket, event):
     mocked_add_event.return_value = True
 
-    db_credentials = {"url": "mocked_url", "token": "mocked_token", "org": "mocked_org"}
-    bucket = "test_bucket"
-    event = Event(deviceId="sensor-1", status="ON", timestamp="2020-01-02T03:44:02")
-
     res = await create_event(db_credentials, bucket, event)
-
-    mocked_add_event.assert_called_once_with(
-        db_credentials,
-        bucket,
-        {
-            "measurement": "sensors",
-            "tags": {"deviceId": "sensor-1"},
-            "fields": {"status": "ON"},
-            "time": "2020-01-02T03:44:02",
-        },
-    )
+    mocked_add_event.assert_called_once_with(db_credentials, bucket, event.data())
     assert res is True
 
 
 @pytest.mark.asyncio
 @patch("app.use_cases.get_status_frequency", new_callable=AsyncMock)
-async def test_get_histogram(mocked_get_status_frequency):
-    mocked_frequency = ["ON", "ON", "OFF", "ACTIVE"]
-    mocked_get_status_frequency.return_value = mocked_frequency
-
-    db_credentials = {"url": "mocked_url", "token": "mocked_token", "org": "mocked_org"}
-    bucket = "test_bucket"
-    query_parameters = {
-        "deviceId": "sensor-1",
-        "start": "2020-01-02T03:44:02",
-        "end": "2020-01-02T03:45:02",
-    }
+async def test_get_histogram(
+    mocked_get_status_frequency, db_credentials, bucket, query_parameters, frequency
+):
+    mocked_get_status_frequency.return_value = frequency
 
     res = await get_histogram(db_credentials, bucket, query_parameters)
     mocked_get_status_frequency.assert_called_once_with(
         db_credentials, get_formatted_flux_query(bucket, query_parameters)
     )
-    assert res == Counter(mocked_frequency)
+    assert res == Counter(frequency)
