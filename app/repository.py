d74f94b6bb6535099b46c089e99b23b61a638968
rafaeltardivo <rafaeldt.br@gmail.com>
2022-04-28 19:49:31 -0300

    Add top n records to repository

--- app/repository.py
+++ app/repository.py
@@ -38,6 +38,7 @@ async def get_status_frequency(
     else:
         query += f" |> range(start: {start}Z)"
     query += f' |> filter(fn:(r) => r.deviceId == "{device_id}")'
+    query += ' |> filter(fn:(r) => r._field == "status")'
 
     async with InfluxDBClientAsync(**credentials) as client:
         query_api = client.query_api()
@@ -48,3 +49,35 @@ async def get_status_frequency(
             frequency.append(value)
 
     return frequency
+
+
+async def get_top_n_records(
+    credentials: dict, bucket: str, device_id: str, indicator: str, limit: int
+) -> list:
+    """Get top n records.
+    Args:
+        credentials (dict): dict containing database credentials.
+        bucket (str): target bucket.
+        device_id (str): device id.
+        indicator (str): device indicator.
+        limit (int): limit for query records..
+    Returns:
+        list: top n ocurrences.
+    """
+    results = []
+
+    query = f' from(bucket:"{bucket}")'
+    query += " |> range(start: 0)"
+    query += f' |> filter(fn:(r) => r.deviceId == "{device_id}")'
+    query += f' |> filter(fn:(r) => r._field == "{indicator}")'
+    query += f" |> top(n: {limit})"
+
+    async with InfluxDBClientAsync(**credentials) as client:
+        query_api = client.query_api()
+        records = await query_api.query_stream(query)
+
+        async for record in records:
+            value = record.get_value()
+            results.append(value)
+
+    return results
